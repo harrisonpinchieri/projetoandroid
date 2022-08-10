86d8b40873379cc71b131ba2526ab2a0aefabf49
harrison <harrisonpinchieri@gmail.com>
2022-08-10 06:58:12 -0300

    themeDataComentado

--- lib/main.dart
+++ lib/main.dart
@@ -17,35 +17,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color.fromARGB(250, 26, 35, 126),
-          titleTextStyle: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ), //TextStyle
-        ), //AppBarThem
-        primarySwatch: Colors.blue,
-
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Color.fromARGB(250, 255, 179, 0),
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(2.0)),
-          ),
-          elevation: 10,
-        ),
-
-        textTheme: const TextTheme(
-          bodyText2: TextStyle(
-              color: Colors.black, fontStyle: FontStyle.italic, fontSize: 20),
-          headline4: TextStyle(
-              color: Color.fromARGB(250, 26, 35, 126),
-              fontWeight: FontWeight.bold,
-              fontSize: 40),
-        ),
-      ),
+      // theme: ThemeData(
+      //   appBarTheme: const AppBarTheme(
+      //     backgroundColor: Color.fromARGB(250, 26, 35, 126),
+      //     titleTextStyle: TextStyle(
+      //       fontSize: 20,
+      //       fontWeight: FontWeight.bold,
+      //       color: Colors.white,
+      //     ), //TextStyle
+      //   ), //AppBarThem
+      //   primarySwatch: Colors.blue,
+      //
+      //   floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      //     backgroundColor: Color.fromARGB(250, 255, 179, 0),
+      //     foregroundColor: Colors.white,
+      //     shape: RoundedRectangleBorder(
+      //       borderRadius: BorderRadius.all(Radius.circular(2.0)),
+      //     ),
+      //     elevation: 10,
+      //   ),
+      //
+      //   textTheme: const TextTheme(
+      //     bodyText2: TextStyle(
+      //         color: Colors.black, fontStyle: FontStyle.italic, fontSize: 20),
+      //     headline4: TextStyle(
+      //         color: Color.fromARGB(250, 26, 35, 126),
+      //         fontWeight: FontWeight.bold,
+      //         fontSize: 40),
+      //   ),
+      // ),
       home: const InitialScreen() ,
     );
   }
